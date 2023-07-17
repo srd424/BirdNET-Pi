12144afae462649bd3cc5bb876b408489dded597
Steve Dodd <steved424@gmail.com>
2023-07-17 22:03:47 +0100

    Explicitly install gnupg, it doesn't get installed with --no-recommends otherwise

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -84,7 +84,7 @@ install_depends() {
   filter_pkg CADDY caddy
   filter_pkg FFMPEG ffmpeg
   if $NEED_CADDY; then
-    apt install -y debian-keyring debian-archive-keyring apt-transport-https
+    apt install -y debian-keyring debian-archive-keyring apt-transport-https gnupg
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
   fi
