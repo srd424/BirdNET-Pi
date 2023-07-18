be18388a24b508fc9fd53343e702aedf5a0f660c
Steve Dodd <steved424@gmail.com>
2023-07-18 19:24:27 +0100

    Push dependency info out into separate file

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -18,24 +18,7 @@ fi
 source ${config_file}
 
 source $my_dir/scripts/set_modules.sh
-
-# PKGS_build in particular can't be split across multiple lines - horrid!
-PKGS_build="swig cmake make libjpeg-dev zlib1g-dev python3-dev python3-pip python3-venv debian-keyring debian-archive-keyring apt-transport-https gnupg"
-
-PKGS_common="sqlite3 sox libsox-fmt-mp3 wget unzip curl bc lsof net-tools"
-
-PKGS_server="${PKGS_common}"
-
-PKGS_main="${PKGS_common} \
-    ftpd php-sqlite3 alsa-utils avahi-utils php php-fpm php-curl php-xml \
-    php-zip icecast2 caddy ffmpeg"
-
-PKGS_local_recording="pulseaudio"
-
-SVCS_server="birdnet_server"
-SVCS_main="birdnet_analysis birdnet_start_server extraction birdnet_recording \
-    caddy avahi-alias@$(hostname).local birdnet_stats spectrogram_viewer \
-  chart_viewer birdnet_log web_terminal icecast2 livestream"
+source $my_dir/scripts/modules_info.sh
 
 [ -n "$MODULES_SKIP_BUILD" ] || MODULES_ENABLED="${MODULES_ENABLED} build"
 
